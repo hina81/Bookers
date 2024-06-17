ad59635a0c6cd74173dabd6c149887699cd90b15
EC2 Default User <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2024-06-17 13:04:57 +0000

    destroy機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'book_edit'
-
+  patch 'books/:id' => 'books#update', as: 'book_update'
+  delete 'books/:id' => 'books#destroy', as: 'book_destroy'
 end
