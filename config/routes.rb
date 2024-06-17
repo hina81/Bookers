905881282ad2956ce7d60b116a43cab974cfcdf7
EC2 Default User <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2024-06-17 02:06:10 +0000

    [finish]showページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   get 'top' => 'homes#top'
-  get 'books' => 'books#index'
+  get 'books' => 'books#index', as: 'book_index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'book_edit'
