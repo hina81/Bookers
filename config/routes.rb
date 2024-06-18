63a18a3c34fa3f5efb1cb25a44520d60c3273b08
EC2 Default User <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2024-06-18 13:15:27 +0000

    レイアウトの作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'book_edit'
   patch 'books/:id' => 'books#update', as: 'book_update'
-  delete 'books/:id' => 'books#destroy', as: 'book_destroy'
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
